f32bfd62985d9affc1f9a62121c47519e4cde550
Abdullah Moqreb <abdallah.mokarb@gmail.com>
2025-04-14 02:18:09 +0200

    fixed navbar anf footer bugs and improve performance in all pages by am

--- JS/RegisterScript.js
+++ JS/RegisterScript.js
@@ -139,3 +139,23 @@ async function registerUser() {
 }
 
 loginBtn.addEventListener("click", registerUser);
+
+// ********************************************
+// *********************************************************
+
+const userCredentials = JSON.parse(localStorage.getItem("userCredentials"));
+
+if (userCredentials) {
+  const loginNav = document.getElementById("loginNavItem");
+  const registerNav = document.getElementById("registerNavItem");
+  if (loginNav) loginNav.style.display = "none";
+  if (registerNav) registerNav.style.display = "none";
+
+  const userNav = document.getElementById("userNavItem");
+  const userNameDisplay = document.getElementById("userNameDisplay");
+
+  if (userNav && userNameDisplay) {
+    userNameDisplay.textContent = `Welcome, ${userCredentials.name}`;
+    userNav.classList.remove("d-none");
+  }
+}
